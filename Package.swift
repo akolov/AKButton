56679b741395bb5624e656b9c844966c198aed0a
Alexander Kolov <me@alexkolov.com>
2021-04-27 18:09:50 +0900

    Update RxSwift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 
 import PackageDescription
 
@@ -13,10 +12,17 @@ let package = Package(
     .library(name: "AKRxButton", targets: ["AKRxButton"])
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.1.1")
+    .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.1.0"))
   ],
   targets: [
     .target(name: "AKButton", dependencies: []),
-    .target(name: "AKRxButton", dependencies: ["AKButton", "RxCocoa", "RxSwift"])
+    .target(
+      name: "AKRxButton",
+      dependencies: [
+        "AKButton",
+        .product(name: "RxCocoa", package: "RxSwift"),
+        .product(name: "RxSwift", package: "RxSwift")
+      ]
+    )
   ]
 )
